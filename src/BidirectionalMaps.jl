9fef93aacb7ec17c0ba35e235fec44f5cb0d8305
Rik Huijzer <rikhuijzer@pm.me>
2021-01-04 14:12:16 +0100

    Also change constructor

--- src/BidirectionalMaps.jl
+++ src/BidirectionalMaps.jl
@@ -8,7 +8,7 @@ struct ImmutableBimap{S,T}
   left::ImmutableDict{S,T}
   right::ImmutableDict{T,S}
 
-  function Bimap{S,T}(U, V) where {S,T}
+  function ImmutableBimap{S,T}(U, V) where {S,T}
       vec2pairs(X, Y) = [first(t) => last(t) for t in zip(X, Y)]
       left = ImmutableDict(vec2pairs(U, V)...)
       right = ImmutableDict(vec2pairs(V, U)...)
